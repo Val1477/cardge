158723f8558a19f999a4f3428305223bb941a38a
Valerie <vfalcone7070@gmail.com>
2019-01-30 01:45:19 +0000

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+import $ from "jquery";
+import "bootstrap"; //breathecode dom for more explicit errors
+//import 'breathecode-dom'; //DOM override to make JS easier to use
+import "../style/index.scss";
+
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
